d8c370f0a4343ac078cdef998faa8a49f8b88946
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2023-11-07 07:23:48 +0100

    Make the toggle button functional and sum moe

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## TODO
 1. parse time from wetherAPI and display it in samsung wether app format - done, though it needs to be somehow interpolated with current time since wetherAPI data is bit hehind (take hours and month from wtherAPI and interpolate new Date() time) or figure something else out</br>
 2. copy samsung wether app rows and display UV etc data.
+3. when after temperature conversion there is 0 in first decimal place, only whats before it.
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const APIKey = '8cae8249f31a48edac8190500230411';
+const toggler = document.querySelector('.toggler > input');
 
 async function getWeatherObj(city) {
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${city}`)
@@ -9,19 +10,30 @@ async function updateDateTime(time) {
     document.querySelector('.timedate').textContent = new Date(time);
 }
 
+const feelsLikeTempSpan = document.querySelector('.feels-like-number');
+const tempNumberSpan = document.querySelector('.temp-number');
+
 async function x(chosenCity='new york') {
+    const toggler = document.querySelector('.toggler > input');
     const cityDiv = document.querySelector('.city')
-    const feelsLikeDiv = document.querySelector('.feels-like-text');
     const conditionDiv = document.querySelector('.condition-text');
-    const tempDiv = document.querySelector('.temp');
     const img = document.querySelector('img');
 
     const wetherDataObj = await getWeatherObj(chosenCity);
     const city = wetherDataObj.location.name;
     const condition = wetherDataObj.current.condition.text;
     const conditionIcon = wetherDataObj.current.condition.icon;
-    const tempC = wetherDataObj.current.temp_c;
-    const feelslikeC = wetherDataObj.current.feelslike_c;
+
+    let temp;
+    let feelslike;
+    if (toggler.checked) {
+        temp =  wetherDataObj.current.temp_f;
+        feelslike = wetherDataObj.current.feelslike_f;
+    } else {
+        temp =  wetherDataObj.current.temp_c;
+        feelslike = wetherDataObj.current.feelslike_c;
+    }
+
     const UV = wetherDataObj.current.uv;
     const humidity = wetherDataObj.current.humidity
     const windKPH = wetherDataObj.current.wind_kph;
@@ -29,14 +41,13 @@ async function x(chosenCity='new york') {
 
     cityDiv.textContent = city;
     updateDateTime(localTime);
-    feelsLikeDiv.textContent = `Feels like ${feelslikeC}°`
-    conditionDiv.textContent = condition
-    tempDiv.textContent = `${tempC}°`
+    feelsLikeTempSpan.textContent = feelslike;
+    conditionDiv.textContent = condition;
+    tempNumberSpan.textContent = temp;
     img.src = `https:${conditionIcon}`
 }
 
 x();
-
 const submitButton = document.querySelector('form button');
 const inputField = document.querySelector('input');
 
@@ -49,5 +60,25 @@ window.onkeydown = (e) => {
     if (e.key == 'Enter') {
         e.preventDefault();
         x(inputField.value);
+    }
+}
+
+function fehrToCelc(fehr) {
+    return ((+fehr - 32) * 5 / 9).toFixed(1);
+}
+
+function celcToFehr(celc) {
+    return (+celc * 9 / 5 + 32).toFixed(1);
+}
+
+toggler.onclick = e => {
+    console.log(e.target.checked);
+    if (e.target.checked) {
+        // temperature is in fehrenheit
+        feelsLikeTempSpan.textContent = fehrToCelc(feelsLikeTempSpan.textContent);
+        tempNumberSpan.textContent = fehrToCelc(tempNumberSpan.textContent);
+    } else {
+        feelsLikeTempSpan.textContent = celcToFehr(feelsLikeTempSpan.textContent);
+        tempNumberSpan.textContent = celcToFehr(tempNumberSpan.textContent);
     }
 }
