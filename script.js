32b8d31e1068b915ea9462d5c90b934f4a65e4fb
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2023-11-19 21:21:46 +0100

    Fix some (hard to find (for me)) bug in fehr => celc and vice versa calculations

--- script.js
+++ script.js
@@ -21,8 +21,13 @@ async function updateDateTime(time) {
 const feelsLikeTempSpan = document.querySelector('.feels-like-number');
 const tempNumberSpan = document.querySelector('.temp-number');
 
+let fehr;
+let celc;
+let feelsLikeFehr;
+let feelsLikeCelc;
+
 async function fillWithData(chosenCity) { // lookup city and display data
-    const toggler = document.querySelector('.toggler > input');
+    const currentToggler = document.querySelector('.toggler > input');
     const cityDiv = document.querySelector('.city')
     const conditionDiv = document.querySelector('.condition-text');
     const img = document.querySelector('img');
@@ -38,15 +43,10 @@ async function fillWithData(chosenCity) { // lookup city and display data
     const condition = wetherDataObj.current.condition.text;
     const conditionIcon = wetherDataObj.current.condition.icon;
 
-    let temp;
-    let feelslike;
-    if (toggler.checked) {
-        temp =  wetherDataObj.current.temp_f;
-        feelslike = wetherDataObj.current.feelslike_f;
-    } else {
-        temp =  wetherDataObj.current.temp_c;
-        feelslike = wetherDataObj.current.feelslike_c;
-    }
+    fehr = wetherDataObj.current.temp_f;
+    feelsLikeFehr = wetherDataObj.current.feelslike_f;
+    celc =  wetherDataObj.current.temp_c;
+    feelsLikeCelc = wetherDataObj.current.feelslike_c;
 
     const UV = wetherDataObj.current.uv;
     const humidity = wetherDataObj.current.humidity
@@ -55,9 +55,14 @@ async function fillWithData(chosenCity) { // lookup city and display data
 
     cityDiv.textContent = city;
     updateDateTime(localTime);
-    feelsLikeTempSpan.textContent = feelslike;
+    if (toggler.checked) {
+        feelsLikeTempSpan.textContent = feelsLikeFehr;
+        tempNumberSpan.textContent = fehr;
+    } else {
+        feelsLikeTempSpan.textContent = feelsLikeCelc;
+        tempNumberSpan.textContent = celc;
+    }
     conditionDiv.textContent = condition;
-    tempNumberSpan.textContent = temp;
     img.src = `https:${conditionIcon}`
 }
 
@@ -78,22 +83,13 @@ form.onkeydown = (e) => {
     }
 }
 
-function fehrToCelc(fehr) {
-    return ((+fehr - 32) * 5 / 9).toFixed(1);
-}
-
-function celcToFehr(celc) {
-    return (+celc * 9 / 5 + 32).toFixed(1);
-}
-
 toggler.onclick = e => {
-    console.log(e.target.checked);
     if (e.target.checked) {
         // temperature is in fehrenheit
-        feelsLikeTempSpan.textContent = fehrToCelc(feelsLikeTempSpan.textContent);
-        tempNumberSpan.textContent = fehrToCelc(tempNumberSpan.textContent);
+        feelsLikeTempSpan.textContent = feelsLikeFehr;
+        tempNumberSpan.textContent = fehr;
     } else {
-        feelsLikeTempSpan.textContent = celcToFehr(feelsLikeTempSpan.textContent);
-        tempNumberSpan.textContent = celcToFehr(tempNumberSpan.textContent);
+        feelsLikeTempSpan.textContent = feelsLikeCelc;
+        tempNumberSpan.textContent = celc;
     }
 }
